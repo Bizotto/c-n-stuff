9d81729f7246d6c683737bdd32e977c21b84aeda
Bizotto <bizotto.nicolas@gmail.com>
2021-09-09 18:57:53 -0300

    exercicio novo

---
+++ LIsta_4/7_posicao.cpp
@@ -0,0 +1,39 @@
+// 7. Leia um vetor de 20 posições e em seguida um valor X qualquer. Seu
+// programa deverá fazer uma busca do valor de X no vetor lido e informar a
+// posição em que foi encontrado ou se não foi encontrado.
+
+// Bibliotecas
+#include <conio.h>
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+
+// Variaveis
+int Vetor[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+int ValorX;
+int Posicao;
+
+// incio
+int main(void)
+{
+    system("cls");
+   
+
+    for (int i = 0; i < 20; i++)
+    {
+        printf("%i\n", Vetor[i]);
+    }
+    printf("\nDigite um numero: ");
+    scanf("\n%i", &ValorX);
+    
+    for (int i = 0; i < 20; i++)
+    {
+        if (ValorX == Vetor[i])
+        {
+            printf("%i", i);
+            return(0);
+        }
+        
+    }
+    
+}
---
+++ LIsta_4/7_vetorX.cpp
@@ -0,0 +1,39 @@
+// 7. Leia um vetor de 20 posições e em seguida um valor X qualquer. Seu
+// programa deverá fazer uma busca do valor de X no vetor lido e informar a
+// posição em que foi encontrado ou se não foi encontrado.
+
+// Bibliotecas
+#include <conio.h>
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+
+// Variaveis
+int Vetor[20];
+int ValorX;
+int QtasVezes;
+
+// incio
+int main(void)
+{
+    srand(time(0));
+    system("cls");
+    for (int i = 0; i < 20; i++)
+    {
+        Vetor[i] = rand() % 20;
+        printf("%i\n", Vetor[i]);
+    }
+    printf("\nDigite um numero qualquer entre 1 e 20\n");
+    scanf("%i", &ValorX);
+
+    QtasVezes = 0;
+    for (int i = 0; i < 20; i++)
+    {
+        if (ValorX == Vetor[i])
+        {
+            QtasVezes = QtasVezes + 1;
+            printf("%i", Vetor[i]);
+        }
+    }
+    printf("\nTotal de vezes en que o Numero aparece = %i", QtasVezes);
+}
