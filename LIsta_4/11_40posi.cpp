1732b11d38106e6dd45052dd0a261c4159ec1881
Bizotto <bizotto.nicolas@gmail.com>
2021-09-14 20:06:37 -0300

    incio de novos exercicios

---
+++ LIsta_4/11_40posi.cpp
@@ -0,0 +1,29 @@
+// 11. Leia um vetor de 40 posições e acumule os valores do primeiro elemento
+// no segundo, deste no terceiro e assim por diante. Ao final, escreva o vetor
+// obtido.
+
+// Bibliotecas
+#include <conio.h>
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+
+// variaveis
+int vetor[40];
+int somavetor[40];
+
+int main(void)
+{
+    srand(time(0));
+    for (int i = 0; i < 40; i++)
+    {
+        vetor[i] = rand() % 100;
+        printf("\n Valor %i Na posicao[%i]", vetor[i], i);
+    }
+    for (int i = 0; i < 40; i = i + 1)
+    {
+        somavetor[i] = vetor[i] + vetor[i];
+
+        printf("\nO resultado da e  %i", somavetor[i]);
+    }
+}
