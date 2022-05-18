b1e9751490bdef4f76682fee4ccb7414e5771ac8
Bizotto <bizotto.nicolas@gmail.com>
2022-05-17 22:39:21 -0300

    ALteracoes na pasta

--- Aula 1/Teste1.cpp
+++ Aula 1/Teste1.cpp
@@ -5,16 +5,17 @@ algoritmo media*/
 #include <conio.h>
 #include <stdio.h>
 
-int N1,N2;
+int N1, N2;
 int Media;
 
-void main(){
+int main()
+{
     printf("\n Entre com o N1:");
-    scanf("%i",&N1);
+    scanf("%i", &N1);
     printf("\n Entre com o N2:");
     scanf("%i", &N2);
-    Media = (N1+N2)/2;
-    
-    printf ("\n A sua media foi %i", Media);
-    printf ("\n Media = (N1: %i + N2: %i )/2 = %i", N1, N2, Media);
+    Media = (N1 + N2) / 2;
+
+    printf("\n A sua media foi %i", Media);
+    printf("\n Media = (N1: %i + N2: %i )/2 = %i", N1, N2, Media);
 }
--- Aula 2/Automovel.cpp
+++ Aula 2/Automovel.cpp
@@ -1,29 +1,30 @@
-//Bibliotecas 
+// Bibliotecas
 
-#include<conio.h> //input... output... comand 
-#include<stdio.h> //Screen comands all of those shit in screen
-#include<iostream> //Variable comands input output
+#include <conio.h>	//input... output... comand
+#include <stdio.h>	//Screen comands all of those shit in screen
+#include <iostream> //Variable comands input output
 
-//Variaveis
+// Variaveis
 char Nomeauto[30];
 float PrecoAuto;
 float PrecoFab;
 
-main (){
-	
+main()
+{
+
 	printf("\n Digite o nome do Automovel: ");
-		scanf("%s",&Nomeauto);
-	
+	scanf("%s", &Nomeauto);
+
 	printf("\n Digite o preco de fabrica do Automovel: ");
-		scanf("%f",&PrecoFab);
-		
-PrecoAuto = (PrecoFab + (PrecoFab*0,45) + (PrecoFab*0.28));
-		
-	printf("\n O Automovel: %s",Nomeauto);
-	
-	printf("\n O Preco final do Automovel: %5.2f",PrecoAuto);
-	
-		printf("\n\n");
-			
-			system("pause");
+	scanf("%f", &PrecoFab);
+
+	PrecoAuto = (PrecoFab + (PrecoFab * 0, 45) + (PrecoFab * 0.28));
+
+	printf("\n O Automovel: %s", Nomeauto);
+
+	printf("\n O Preco final do Automovel: %5.2f", PrecoAuto);
+
+	printf("\n\n");
+
+	system("pause");
 }
